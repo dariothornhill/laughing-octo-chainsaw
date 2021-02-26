545aea33771a4df6b5d317f4cacd05be5d27ff5e
Dario Thornhill <dario.thornhill@gmail.com>
2021-02-27 00:59:27 +0100

    Added port env variable

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const app = require('express')();
 const cors = require('cors');
 
+const PORT = process.env.PORT || 3000;
+
 app.use(cors());
 app.get('/*', (req, res, next) => {
     console.log(req);
     res.send('ok');
 })
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
 })
